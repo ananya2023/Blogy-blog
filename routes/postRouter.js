2dba0f176d1b6a907881dd1050c105a29d69594a
ananya <ananya.miriyala474@gmail.com>
2022-04-27 14:11:38 +0530

    fc

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -1,26 +1,28 @@
-const express = require('express');
+const express = require("express");
 const {
   getAllPosts,
   getPosts,
   createPost,
   updatePost,
   deletePost,
-} = require('../controllers/postController');
+  createcomment,
+  getcomment,
+} = require("../controllers/postController");
 
-const { admin } = require('../middleware/authMiddleware');
+const { admin } = require("../middleware/authMiddleware");
 
-const { protect } = require('../middleware/authMiddleware');
+const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-router.route('/').get(getAllPosts);
+router.route("/").get(getAllPosts);
 
-router.route('/:id').get(getPosts);
+router.route("/:id").get(getPosts);
 
-router.route('/post-create').post(protect, createPost);
+router.route("/post-create").post(protect, createPost);
 
-router.route('/:id').put(protect, updatePost);
+router.route("/").post(createcomment); //?
 
-router.route('/:id').delete(protect, admin, deletePost);
+// router.route('/:id').get(protect,getcomment);
 
-module.exports = router;
+module.exports =  router ;
